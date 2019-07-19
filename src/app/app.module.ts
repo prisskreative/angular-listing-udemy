e8fbf5667d9f1a1ed1791c2df8c6e9b00435c743
Priscilla <priss@prisskreative.com>
2019-07-19 15:03:36 -0500

    create crib card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CribListingComponent } from './crib-listing/crib-listing.component';
+import { CribCardComponent } from './crib-card/crib-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CribListingComponent
+    CribListingComponent,
+    CribCardComponent
   ],
   imports: [
     BrowserModule,
