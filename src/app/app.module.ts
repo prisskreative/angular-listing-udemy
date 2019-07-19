975ba9bec2ccca78ff603d38eddd62bd072edf1d
Priscilla <priss@prisskreative.com>
2019-07-19 11:52:42 -0500

    Create crib listing component with cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CribListingComponent } from './crib-listing/crib-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CribListingComponent
   ],
   imports: [
     BrowserModule,
